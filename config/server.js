8f84bb94b69f292ac2a92289d32ec6d4a65932ce
Falcao\Kevin <santoskevin032@gmail.com>
2021-04-22 09:56:32 -0300

    aaaa

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3bb8f400f7402bd2b5ef6082cbcaa421'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1aa07095-dbe9-489b-bc86-c814fc5386a5'
+};
